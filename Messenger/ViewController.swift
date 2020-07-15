a33906517b95a415c80762ed1eb02209966d64c9
Syrym Zhursin <jurssin@gmail.com>
2020-07-15 16:08:41 +0600

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by User on 7/15/20.
+//  Copyright © 2020 Syrym Zhursin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
