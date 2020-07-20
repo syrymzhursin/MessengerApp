9384ccd0ffc8fe2390dd0f2fd858e361e2edaa8f
Syrym Zhursin <jurssin@gmail.com>
2020-07-20 12:32:25 +0600

    DatabaseManagerChange

--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -26,7 +26,11 @@ final class DatabaseManager {
 extension DatabaseManager {
     
     public func userExists(with email: String, completion: @escaping ((Bool) -> Void))  {
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -37,7 +41,7 @@ extension DatabaseManager {
     
     /// Inserts new user to database
     public func insertUser(with user: ChatAppUser) {
-           database.child(user.email).setValue([
+           database.child(user.safeEmail).setValue([
                "first_name": user.firstName,
                "last_name": user.lastName
            ])
@@ -48,6 +52,13 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let email: String
+    
+    var safeEmail: String {
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        return safeEmail
+    }
 //  let profilePictureUrl: String
 }
 
